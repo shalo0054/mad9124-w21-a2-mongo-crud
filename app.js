e79776343fd49a48a02e9103d49ecd7207580648
Karim Shaloh <shal0054@algonquinlive.com>
2021-03-07 23:25:01 -0400

    Assignment complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const debug = require('debug')('mad9124-w21-a2-mongo-crud');
 const morgan = require('morgan');
 const express = require('express');
+const sanitizeMongo = require('express-mongo-sanitize');
 
 require('./startup/database.js');
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/courses', require('./routes/courses.js'));
 app.use('/api/students', require('./routes/students.js'));
+app.use(sanitizeMongo());
 
 // start listening for HTTP requests
 const port = process.env.port || 3030;
